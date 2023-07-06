d487e93ff98d37acec5c0c756193a274faaa00d0
GreatenAnoymous <2274880117@qq.com>
2023-07-06 09:40:27 -0400

    ecbs

--- include/focal_hybrid_astar.hpp
+++ include/focal_hybrid_astar.hpp
@@ -17,15 +17,13 @@
 namespace libMultiRobotPlanning
 {
 
-    const double sub_optimality = 1.5;
-
     template <typename State, typename Action, typename Cost, typename Environment,
               typename StateHasher = std::hash<State>>
 
     class FocalHybridAStar
     {
     public:
-        FocalHybridAStar(Environment &environment, float w = 1.5) : m_nev(environment), m_w(w) {}
+        FocalHybridAStar(Environment &environment, float w = 1.5) : m_env(environment), m_w(w) {}
         ~FocalHybridAStar() {}
 
         bool search(const State &startState, PlanResult<State, Action, Cost> &solution)
@@ -35,16 +33,16 @@ namespace libMultiRobotPlanning
             solution.cost = 0;
             openSet_t openSet;
             focalSet_t focalSet;
-            std::unordered_map<uint64_t, heapHandle_t, std::hash<uint64_t>> stateToHeap;
+            std::unordered_map<uint64_t, fibHeapHandle_t, std::hash<uint64_t>> stateToHeap;
             std::unordered_set<uint64_t, std::hash<uint64_t>> closedSet;
             std::unordered_map<State, std::tuple<State, Action, Cost, Cost>,
                                StateHasher>
                 cameFrom;
 
             auto handle = openSet.push(Node(startState, Action(),
-                                            m_env.admissibleHeuristic(startState), initialCost));
+                                            m_env.admissibleHeuristic(startState), 0,0));
 
-            stateToHeap.insert(std::make_pair<>(startState, handle));
+            stateToHeap.insert(std::make_pair<>(m_env.calcIndex(startState), handle));
             (*handle).handle = handle;
             focalSet.push(handle);
             std::vector<Neighbor<State, Action, Cost>> neighbors;
@@ -104,7 +102,7 @@ namespace libMultiRobotPlanning
                             std::get<1>(iter->second), std::get<2>(iter->second)));
                         iter = cameFrom.find(std::get<0>(iter->second));
                     }
-                    solution.states.push_back(std::make_pair<>(startState, initialCost));
+                    solution.states.push_back(std::make_pair<>(startState, 0));
                     std::reverse(solution.states.begin(), solution.states.end());
                     std::reverse(solution.actions.begin(), solution.actions.end());
 
@@ -119,22 +117,115 @@ namespace libMultiRobotPlanning
 
                 // traverse neighbors
                 neighbors.clear();
-                m_env.getNeighbors(current.state, neighbors);
-
+                m_env.getNeighbors(current.state, current.action, neighbors);
 
                 //
-                for (const Neighbor<State, Action, Cost> &neighbor : neighbors){
-                    
+                for (const Neighbor<State, Action, Cost> &neighbor : neighbors)
+                {
+                    if (closedSet.find(m_env.calcIndex(neighbor.state)) ==
+                        closedSet.end())
+                    { // not in closed set
+                        Cost tentative_gScore = current.gScore + neighbor.cost;
+                        auto iter = stateToHeap.find(m_env.calcIndex(neighbor.state));
+                        if (iter == stateToHeap.end())
+                        { // Discover a new node
+                            Cost fScore =
+                                tentative_gScore + m_env.admissibleHeuristic(neighbor.state);
+
+                            Cost focalHeuristic =
+                                current.focalHeuristic +
+                                m_env.focalStateHeuristic(neighbor.state, tentative_gScore) +
+                                m_env.focalTransitionHeuristic(current.state, neighbor.state,
+                                                               current.gScore,
+                                                               tentative_gScore);
+                            auto handle = openSet.push(Node(neighbor.state, neighbor.action,
+                                                                 fScore, tentative_gScore, focalHeuristic));
+                            (*handle).handle = handle;
+                            if (fScore <= bestFScore * m_w)
+                            {
+                                // std::cout << "focalAdd: " << *handle << std::endl;
+                                focalSet.push(handle);
+                            }
+                            stateToHeap.insert(
+
+                                std::make_pair<>(m_env.calcIndex(neighbor.state), handle));
+                            m_env.onDiscover(neighbor.state, fScore, tentative_gScore);
+                            // std::cout << "  this is a new node " << fScore << "," <<
+                            // tentative_gScore << std::endl;
+                        }
+                        else
+                        {
+                            auto handle = iter->second;
+                            // std::cout << "  this is an old node: " << tentative_gScore << ","
+                            // << (*handle).gScore << std::endl;
+                            // We found this node before with a better path
+                            if (tentative_gScore >= (*handle).gScore)
+                            {
+                                continue;
+                            }
+
+                            // update f and gScore
+                            // Cost last_gScore = (*handle).gScore;
+                            Cost last_fScore = (*handle).fScore;
+                            Cost delta = (*handle).gScore - tentative_gScore;
+                            (*handle).gScore = tentative_gScore;
+                            (*handle).fScore -= delta;
+                            (*handle).state = neighbor.state;
+                            openSet.increase(handle);
+                            m_env.onDiscover(neighbor.state, (*handle).fScore,
+                                             (*handle).gScore);
+                            if ((*handle).fScore <= bestFScore * m_w &&
+                                last_fScore > bestFScore * m_w)
+                            {
+                                // std::cout << "focalAdd: " << *handle << std::endl;
+                                focalSet.push(handle);
+                            }
+                        }
+
+                        // Best path for this node so far
+                        // TODO: this is not the best way to update "cameFrom", but otherwise
+                        // default c'tors of State and Action are required
+                        cameFrom.erase(neighbor.state);
+                        cameFrom.insert(std::make_pair<>(
+                            neighbor.state,
+                            std::make_tuple<>(current.state, neighbor.action, neighbor.cost,
+                                              tentative_gScore)));
+                    }
                 }
+            }
+            openSet.clear();
+            return false;
         }
 
     private:
-        struct FocalNode
+        struct Node;
+
+#ifdef USE_FIBONACCI_HEAP
+        typedef typename boost::heap::fibonacci_heap<Node> openSet_t;
+        typedef typename openSet_t::handle_type fibHeapHandle_t;
+// typedef typename boost::heap::fibonacci_heap<fibHeapHandle_t,
+// boost::heap::compare<compareFocalHeuristic> > focalSet_t;
+#else
+        typedef typename boost::heap::d_ary_heap<Node, boost::heap::arity<2>,
+                                                 boost::heap::mutable_<true>>
+            openSet_t;
+        typedef typename openSet_t::handle_type fibHeapHandle_t;
+// typedef typename boost::heap::d_ary_heap<fibHeapHandle_t,
+// boost::heap::arity<2>, boost::heap::mutable_<true>,
+// boost::heap::compare<compareFocalHeuristic> > focalSet_t;
+#endif
+        struct Node
         {
+           
+            Node(const State &state, Action action, Cost fScore, Cost gScore, Cost focalHeuristic) : state(state), action(action),
+                                                                                           fScore(fScore),
+                                                                                           gScore(gScore),
+                                                                                           focalHeuristic(focalHeuristic) {}
+
             friend std::ostream &operator<<(std::ostream &os, const Node &node)
             {
                 os << "state: " << node.state << " fScore: " << node.fScore
-                   << " gScore: " << node.gScore;
+                   << " gScore: " << node.gScore << " focal: " << node.focalHeuristic;
                 return os;
             }
 
@@ -142,7 +233,56 @@ namespace libMultiRobotPlanning
             Action action;
             Cost fScore;
             Cost gScore;
-        }
+            Cost focalHeuristic;
+
+            bool operator<(const Node &other) const
+            {
+                // Sort order
+                // 1. lowest fScore
+                // 2. highest gScore
+
+                // Our heap is a maximum heap, so we invert the comperator function here
+                if (fScore != other.fScore)
+                {
+                    return fScore > other.fScore;
+                }
+                else
+                {
+                    return gScore < other.gScore;
+                }
+            }
+
+            fibHeapHandle_t handle;
+        };
+
+        struct compareFocalHeuristic
+        {
+            bool operator()(const fibHeapHandle_t &h1,
+                            const fibHeapHandle_t &h2) const
+            {
+                // Sort order (see "Improved Solvers for Bounded-Suboptimal Multi-Agent
+                // Path Finding" by Cohen et. al.)
+                // 1. lowest focalHeuristic
+                // 2. lowest fScore
+                // 3. highest gScore
+
+                // Our heap is a maximum heap, so we invert the comperator function here
+                if ((*h1).focalHeuristic != (*h2).focalHeuristic)
+                {
+                    return (*h1).focalHeuristic > (*h2).focalHeuristic;
+                    // } else if ((*h1).fScore != (*h2).fScore) {
+                    //   return (*h1).fScore > (*h2).fScore;
+                }
+                else if ((*h1).fScore != (*h2).fScore)
+                {
+                    return (*h1).fScore > (*h2).fScore;
+                }
+                else
+                {
+                    return (*h1).gScore < (*h2).gScore;
+                }
+            }
+        };
 
 #ifdef USE_FIBONACCI_HEAP
         typename boost::heap::fibonacci_heap<Node>::handle_type handle;
@@ -152,15 +292,7 @@ namespace libMultiRobotPlanning
             handle;
 #endif
 
-#ifdef USE_FIBONACCI_HEAP
-        typedef typename boost::heap::fibonacci_heap<Node> openSet_t;
-        typedef typename openSet_t::handle_type heapHandle_t;
-#else
-        typedef typename boost::heap::d_ary_heap<Node, boost::heap::arity<2>,
-                                                 boost::heap::mutable_<true>>
-            openSet_t;
-        typedef typename openSet_t::handle_type heapHandle_t;
-#endif
+
 
 #ifdef USE_FIBONACCI_HEAP
         // typedef typename boost::heap::fibonacci_heap<Node> openSet_t;
@@ -179,8 +311,7 @@ namespace libMultiRobotPlanning
 #endif
 
     private:
-        Environment &m_nev;
+        Environment &m_env;
         double m_w;
     };
-
-}
+};
--- src/cl_cbs.cpp
+++ src/cl_cbs.cpp
@@ -30,39 +30,48 @@ using namespace libMultiRobotPlanning;
 // calculate agent collision more precisely BUT need LONGER time
 // #define PRCISE_COLLISION
 
-struct Location {
+struct Location
+{
   Location(double x, double y) : x(x), y(y) {}
   double x;
   double y;
 
-  bool operator<(const Location& other) const {
+  bool operator<(const Location &other) const
+  {
     return std::tie(x, y) < std::tie(other.x, other.y);
   }
 
-  bool operator==(const Location& other) const {
+  bool operator==(const Location &other) const
+  {
     return std::tie(x, y) == std::tie(other.x, other.y);
   }
 
-  friend std::ostream& operator<<(std::ostream& os, const Location& c) {
+  friend std::ostream &operator<<(std::ostream &os, const Location &c)
+  {
     return os << "(" << c.x << "," << c.y << ")";
   }
 };
 
-namespace std {
-template <>
-struct hash<Location> {
-  size_t operator()(const Location& s) const {
-    size_t seed = 0;
-    boost::hash_combine(seed, s.x);
-    boost::hash_combine(seed, s.y);
-    return seed;
-  }
-};
-}  // namespace std
+namespace std
+{
+  template <>
+  struct hash<Location>
+  {
+    size_t operator()(const Location &s) const
+    {
+      size_t seed = 0;
+      boost::hash_combine(seed, s.x);
+      boost::hash_combine(seed, s.y);
+      return seed;
+    }
+  };
+} // namespace std
 
-struct State {
+struct State
+{
   State(double x, double y, double yaw, int time = 0)
-      : time(time), x(x), y(y), yaw(yaw) {
+      : time(time), x(x), y(y), yaw(yaw)
+  {
     rot.resize(2, 2);
     rot(0, 0) = cos(-this->yaw);
     rot(0, 1) = -sin(-this->yaw);
@@ -110,11 +119,13 @@ struct State {
 
   State() = default;
 
-  bool operator==(const State& s) const {
+  bool operator==(const State &s) const
+  {
     return std::tie(time, x, y, yaw) == std::tie(s.time, s.x, s.y, s.yaw);
   }
 
-  bool agentCollision(const State& other) const {
+  bool agentCollision(const State &other) const
+  {
 #ifndef PRCISE_COLLISION
     if (pow(this->x - other.x, 2) + pow(this->y - other.y, 2) <
         pow(2 * Constants::LF, 2) + pow(Constants::carWidth, 2))
@@ -130,14 +141,17 @@ struct State {
                                     Segment(other.corner3, other.corner4),
                                     Segment(other.corner4, other.corner1)};
     for (auto seg1 = rectangle1.begin(); seg1 != rectangle1.end(); seg1++)
-      for (auto seg2 = rectangle2.begin(); seg2 != rectangle2.end(); seg2++) {
-        if (boost::geometry::intersects(*seg1, *seg2)) return true;
+      for (auto seg2 = rectangle2.begin(); seg2 != rectangle2.end(); seg2++)
+      {
+        if (boost::geometry::intersects(*seg1, *seg2))
+          return true;
       }
     return false;
 #endif
   }
 
-  bool obsCollision(const Location& obstacle) const {
+  bool obsCollision(const Location &obstacle) const
+  {
     boost::numeric::ublas::matrix<double> obs(1, 2);
     obs(0, 0) = obstacle.x - this->x;
     obs(0, 1) = obstacle.y - this->y;
@@ -151,7 +165,8 @@ struct State {
     return false;
   }
 
-  friend std::ostream& operator<<(std::ostream& os, const State& s) {
+  friend std::ostream &operator<<(std::ostream &os, const State &s)
+  {
     return os << "(" << s.x << "," << s.y << ":" << s.yaw << ")@" << s.time;
   }
 
@@ -160,28 +175,32 @@ struct State {
   double y;
   double yaw;
 
- private:
+private:
   boost::numeric::ublas::matrix<double> rot;
   Point corner1, corner2, corner3, corner4;
 };
 
-namespace std {
-template <>
-struct hash<State> {
-  size_t operator()(const State& s) const {
-    size_t seed = 0;
-    boost::hash_combine(seed, s.time);
-    boost::hash_combine(seed, s.x);
-    boost::hash_combine(seed, s.y);
-    boost::hash_combine(seed, s.yaw);
-    return seed;
-  }
-};
-}  // namespace std
+namespace std
+{
+  template <>
+  struct hash<State>
+  {
+    size_t operator()(const State &s) const
+    {
+      size_t seed = 0;
+      boost::hash_combine(seed, s.time);
+      boost::hash_combine(seed, s.x);
+      boost::hash_combine(seed, s.y);
+      boost::hash_combine(seed, s.yaw);
+      return seed;
+    }
+  };
+} // namespace std
 
-using Action = int;  // int<7 int ==6 wait
+using Action = int; // int<7 int ==6 wait
 
-struct Conflict {
+struct Conflict
+{
   int time;
   size_t agent1;
   size_t agent2;
@@ -189,14 +208,16 @@ struct Conflict {
   State s1;
   State s2;
 
-  friend std::ostream& operator<<(std::ostream& os, const Conflict& c) {
+  friend std::ostream &operator<<(std::ostream &os, const Conflict &c)
+  {
     os << c.time << ": Collision [ " << c.agent1 << c.s1 << " , " << c.agent2
        << c.s2 << " ]";
     return os;
   }
 };
 
-struct Constraint {
+struct Constraint
+{
   Constraint(int time, State s, size_t agentid)
       : time(time), s(s), agentid(agentid) {}
   Constraint() = default;
@@ -204,24 +225,28 @@ struct Constraint {
   State s;
   size_t agentid;
 
-  bool operator<(const Constraint& other) const {
+  bool operator<(const Constraint &other) const
+  {
     return std::tie(time, s.x, s.y, s.yaw, agentid) <
            std::tie(other.time, other.s.x, other.s.y, other.s.yaw,
                     other.agentid);
   }
 
-  bool operator==(const Constraint& other) const {
+  bool operator==(const Constraint &other) const
+  {
     return std::tie(time, s.x, s.y, s.yaw, agentid) ==
            std::tie(other.time, other.s.x, other.s.y, other.s.yaw,
                     other.agentid);
   }
 
-  friend std::ostream& operator<<(std::ostream& os, const Constraint& c) {
+  friend std::ostream &operator<<(std::ostream &os, const Constraint &c)
+  {
     return os << "Constraint[" << c.time << "," << c.s << "from " << c.agentid
               << "]";
   }
 
-  bool satisfyConstraint(const State& state) const {
+  bool satisfyConstraint(const State &state) const
+  {
     if (state.time < this->time ||
         state.time > this->time + Constants::constraintWaitTime)
       return true;
@@ -229,51 +254,65 @@ struct Constraint {
   }
 };
 
-namespace std {
-template <>
-struct hash<Constraint> {
-  size_t operator()(const Constraint& s) const {
-    size_t seed = 0;
-    boost::hash_combine(seed, s.time);
-    boost::hash_combine(seed, s.s.x);
-    boost::hash_combine(seed, s.s.y);
-    boost::hash_combine(seed, s.s.yaw);
-    boost::hash_combine(seed, s.agentid);
-    return seed;
-  }
-};
-}  // namespace std
+namespace std
+{
+  template <>
+  struct hash<Constraint>
+  {
+    size_t operator()(const Constraint &s) const
+    {
+      size_t seed = 0;
+      boost::hash_combine(seed, s.time);
+      boost::hash_combine(seed, s.s.x);
+      boost::hash_combine(seed, s.s.y);
+      boost::hash_combine(seed, s.s.yaw);
+      boost::hash_combine(seed, s.agentid);
+      return seed;
+    }
+  };
+} // namespace std
 
 // FIXME: modidy data struct, it's not the best option
-struct Constraints {
+struct Constraints
+{
   std::unordered_set<Constraint> constraints;
 
-  void add(const Constraints& other) {
+  void add(const Constraints &other)
+  {
     constraints.insert(other.constraints.begin(), other.constraints.end());
   }
 
-  bool overlap(const Constraints& other) {
-    for (const auto& c : constraints) {
-      if (other.constraints.count(c) > 0) return true;
+  bool overlap(const Constraints &other)
+  {
+    for (const auto &c : constraints)
+    {
+      if (other.constraints.count(c) > 0)
+        return true;
     }
     return false;
   }
 
-  friend std::ostream& operator<<(std::ostream& os, const Constraints& cs) {
-    for (const auto& c : cs.constraints) {
+  friend std::ostream &operator<<(std::ostream &os, const Constraints &cs)
+  {
+    for (const auto &c : cs.constraints)
+    {
       os << c << std::endl;
     }
     return os;
   }
 };
 
-void readAgentConfig() {
+void readAgentConfig()
+{
   YAML::Node car_config;
   std::string test(__FILE__);
   boost::replace_all(test, "cl_cbs.cpp", "config.yaml");
-  try {
+  try
+  {
     car_config = YAML::LoadFile(test.c_str());
-  } catch (std::exception& e) {
+  }
+  catch (std::exception &e)
+  {
     std::cerr << "\033[1m\033[33mWARNING: Failed to load agent config file: "
               << test << "\033[0m , Using default params. \n";
   }
@@ -309,11 +348,12 @@ void readAgentConfig() {
                    0,
                    -Constants::r * (1 - cos(Constants::deltat)),
                    Constants::r * (1 - cos(Constants::deltat))};
-  Constants::dyaw = {0, Constants::deltat,  -Constants::deltat,
+  Constants::dyaw = {0, Constants::deltat, -Constants::deltat,
                      0, -Constants::deltat, Constants::deltat};
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   namespace po = boost::program_options;
   // Declare the supported options.
   po::options_description desc("Allowed options");
@@ -328,17 +368,22 @@ int main(int argc, char* argv[]) {
       "batchsize,b", po::value<int>(&batchSize)->default_value(10),
       "batch size for iter");
 
-  try {
+  try
+  {
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
 
-    if (vm.count("help") != 0u) {
+    if (vm.count("help") != 0u)
+    {
       std::cout << desc << "\n";
       return 0;
     }
-  } catch (po::error& e) {
-    std::cerr << e.what() << std::endl << std::endl;
+  }
+  catch (po::error &e)
+  {
+    std::cerr << e.what() << std::endl
+              << std::endl;
     std::cerr << desc << std::endl;
     return 1;
   }
@@ -346,14 +391,17 @@ int main(int argc, char* argv[]) {
   readAgentConfig();
 
   YAML::Node map_config;
-  try {
+  try
+  {
     map_config = YAML::LoadFile(inputFile);
-  } catch (std::exception& e) {
+  }
+  catch (std::exception &e)
+  {
     std::cerr << "\033[1m\033[31mERROR: Failed to load map file: " << inputFile
               << "\033[0m \n";
     return 0;
   }
-  const auto& dim = map_config["map"]["dimensions"];
+  const auto &dim = map_config["map"]["dimensions"];
   int dimx = dim[0].as<int>();
   int dimy = dim[1].as<int>();
 
@@ -361,12 +409,14 @@ int main(int argc, char* argv[]) {
   std::multimap<int, State> dynamic_obstacles;
   std::vector<State> goals;
   std::vector<State> startStates;
-  for (const auto& node : map_config["map"]["obstacles"]) {
+  for (const auto &node : map_config["map"]["obstacles"])
+  {
     obstacles.insert(Location(node[0].as<double>(), node[1].as<double>()));
   }
-  for (const auto& node : map_config["agents"]) {
-    const auto& start = node["start"];
-    const auto& goal = node["goal"];
+  for (const auto &node : map_config["agents"])
+  {
+    const auto &start = node["start"];
+    const auto &goal = node["goal"];
     startStates.emplace_back(State(start[0].as<double>(), start[1].as<double>(),
                                    start[2].as<double>()));
     // std::cout << "s: " << startStates.back() << std::endl;
@@ -378,10 +428,12 @@ int main(int argc, char* argv[]) {
   double timer = 0;
   bool success = false;
   std::vector<PlanResult<State, Action, double>> solution;
-  for (size_t iter = 0; iter < (double)goals.size() / batchSize; iter++) {
+  for (size_t iter = 0; iter < (double)goals.size() / batchSize; iter++)
+  {
     size_t first = iter * batchSize;
     size_t last = first + batchSize;
-    if (last >= goals.size()) last = goals.size();
+    if (last >= goals.size())
+      last = goals.size();
     std::vector<State> m_goals(goals.begin() + first, goals.begin() + last);
     std::vector<State> m_starts(startStates.begin() + first,
                                 startStates.begin() + last);
@@ -389,11 +441,13 @@ int main(int argc, char* argv[]) {
     Environment<Location, State, Action, double, Conflict, Constraint,
                 Constraints>
         mapf(dimx, dimy, obstacles, dynamic_obstacles, m_goals);
-    if (!mapf.startAndGoalValid(m_starts, iter, batchSize)) {
+    if (!mapf.startAndGoalValid(m_starts, iter, batchSize))
+    {
       success = false;
       break;
     }
-    for (auto goal = goals.begin() + last; goal != goals.end(); goal++) {
+    for (auto goal = goals.begin() + last; goal != goals.end(); goal++)
+    {
       dynamic_obstacles.insert(
           std::pair<int, State>(-1, State(goal->x, goal->y, goal->yaw)));
     }
@@ -406,14 +460,18 @@ int main(int argc, char* argv[]) {
     success = cbsHybrid.search(m_starts, m_solution);
     iterTimer.stop();
 
-    if (!success) {
+    if (!success)
+    {
       std::cout << "\033[1m\033[31m No." << iter
                 << "iter fail to find a solution \033[0m\n";
       break;
-    } else {
+    }
+    else
+    {
       solution.insert(solution.end(), m_solution.begin(), m_solution.end());
-      for (size_t a = 0; a < m_solution.size(); ++a) {
-        for (const auto& state : m_solution[a].states)
+      for (size_t a = 0; a < m_solution.size(); ++a)
+      {
+        for (const auto &state : m_solution[a].states)
           dynamic_obstacles.insert(std::pair<int, State>(
               state.first.time,
               State(state.first.x, state.first.y, state.first.yaw)));
@@ -436,16 +494,20 @@ int main(int argc, char* argv[]) {
   std::ofstream out;
   out = std::ofstream(outputFile);
 
-  if (success) {
+  if (success)
+  {
     std::cout << "\033[1m\033[32m Successfully find solution! \033[0m\n";
 
     double makespan = 0, flowtime = 0, cost = 0;
-    for (const auto& s : solution) cost += s.cost;
+    for (const auto &s : solution)
+      cost += s.cost;
 
-    for (size_t a = 0; a < solution.size(); ++a) {
+    for (size_t a = 0; a < solution.size(); ++a)
+    {
       // calculate makespan
       double current_makespan = 0;
-      for (size_t i = 0; i < solution[a].actions.size(); ++i) {
+      for (size_t i = 0; i < solution[a].actions.size(); ++i)
+      {
         // some action cost have penalty coefficient
 
         if (solution[a].actions[i].second < Constants::dx[0])
@@ -456,7 +518,8 @@ int main(int argc, char* argv[]) {
           current_makespan += Constants::r * Constants::deltat;
       }
       flowtime += current_makespan;
-      if (current_makespan > makespan) makespan = current_makespan;
+      if (current_makespan > makespan)
+        makespan = current_makespan;
     }
     std::cout << " Runtime: " << timer << std::endl
               << " Makespan:" << makespan << std::endl
@@ -469,7 +532,8 @@ int main(int argc, char* argv[]) {
     out << "  flowtime: " << flowtime << std::endl;
     out << "  runtime: " << timer << std::endl;
     out << "schedule:" << std::endl;
-    for (size_t a = 0; a < solution.size(); ++a) {
+    for (size_t a = 0; a < solution.size(); ++a)
+    {
       // std::cout << "Solution for: " << a << std::endl;
       // for (size_t i = 0; i < solution[a].actions.size(); ++i) {
       //   std::cout << solution[a].states[i].second << ": "
@@ -481,15 +545,21 @@ int main(int argc, char* argv[]) {
       // std::cout << solution[a].states.back().second << ": "
       //           << solution[a].states.back().first << std::endl;
 
+
+      // check the 
+
       out << "  agent" << a << ":" << std::endl;
-      for (const auto& state : solution[a].states) {
+      for (const auto &state : solution[a].states)
+      {
         out << "    - x: " << state.first.x << std::endl
             << "      y: " << state.first.y << std::endl
             << "      yaw: " << state.first.yaw << std::endl
             << "      t: " << state.first.time << std::endl;
       }
     }
-  } else {
+  }
+  else
+  {
     std::cout << "\033[1m\033[31m Fail to find paths \033[0m\n";
   }
 }
